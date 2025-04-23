1888d89784f0e04dec3c89fbe25ad4951c1c143c
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-23 10:06:49 +0200

    solution

--- src/main/scala/Allergies.scala
+++ src/main/scala/Allergies.scala
@@ -1,21 +1,18 @@
 import Allergen.Strawberries
 
 enum Allergen(val score: Int):
-   case Eggs extends Allergen(1) // 1
-   case Peanuts extends Allergen(2) // 10
-   case Shellfish extends Allergen(4) // 100
-   case Strawberries extends Allergen(8) // 1.000
-   case Tomatoes extends Allergen(16) // 10.000
-   case Chocolate extends Allergen(32) // 100.000
-   case Pollen extends Allergen(64) // 1.000.000
-   case Cats extends Allergen(128) // 10.000.000
+   case Eggs extends Allergen(1)
+   case Peanuts extends Allergen(2)
+   case Shellfish extends Allergen(4)
+   case Strawberries extends Allergen(8)
+   case Tomatoes extends Allergen(16)
+   case Chocolate extends Allergen(32)
+   case Pollen extends Allergen(64)
+   case Cats extends Allergen(128)
 
 object Allergies:
    def list(n: Int): List[Allergen] =
-      println(s"Scoring $n ('${n.toBinaryString}'), which allergens are you allergic to?")
-      if n == 0 then Nil
-      else if n == Allergen.values.map(_.score).sum then Allergen.values.toList
-      else Allergen.values.filter(i => i.score == n).toList
+      Allergen.values.zip(n.toBinaryString.reverse).filter(_._2 == '1').map(_._1).toList
 
    def allergicTo(a: Allergen, n: Int): Boolean =
-      if n == 0 then false else Allergen.values.map(_.score).contains(n - a.score) || n == a.score
+      Allergen.values.map(_.score).contains(n - a.score) || n == a.score && n != 0
